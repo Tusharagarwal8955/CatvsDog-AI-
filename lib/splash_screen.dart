454e735f4dfb3c2f677d86487f4a0584f375483c
Tusharagarwal8955 <tushar.agarwal233@gmail.com>
2020-10-10 13:10:22 +0530

    Final Commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+import 'home.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text("Cat vs Dog",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Color(0xFFE99600))),
+      photoSize: 50,
+      image: Image.asset("assets/cat.png"),
+      backgroundColor: Colors.black,
+      loaderColor: Color(0xffeeda28),
+    );
+  }
+}
